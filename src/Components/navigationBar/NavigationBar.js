2e48f27b0a51e995379f24f87adf10f2c9952321
Shivani Borhara <shivaniborhara@shivanis-mbp.lan>
2022-09-08 17:46:07 -0400

    nav css styling added

--- src/Components/navigationBar/NavigationBar.js
+++ src/Components/navigationBar/NavigationBar.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import './navigationBar.css';
+
 import { Navbar, Nav } from 'react-bootstrap';
 import { navigationLinks } from '../../helpers/navigationLinks';
+
+let createLinks = () => {
+	return navigationLinks.map((element, index) => (
+		<Nav.Link key={index} href={element.ref}>
+			{element.name}
+		</Nav.Link>
+	));
+};
 
 function NavigationBar() {
 	return (
 		<div id="home">
-			<Navbar>
-				<Navbar.Brand href="#home">Shivani M Borhara</Navbar.Brand>
+			<Navbar className="navigation__container" expand="md">
+				<Navbar.Brand style={{ marginLeft: '1rem' }} href="#home">
+					Shivani M Borhara
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse
+					style={{
+						justifyContent: 'flex-end',
+						marginRight: '1rem',
+						borderColor: 'none',
+					}}
+				>
+					<Nav style={{ margin: '0 1rem' }} className="links">
+						{createLinks()}
+					</Nav>
+				</Navbar.Collapse>
 			</Navbar>
 		</div>
 	);
