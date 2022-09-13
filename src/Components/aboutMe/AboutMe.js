14b7b461ed32f5c2683ab373a3c83af02a25c766
Shivani Borhara <shivaniborhara@shivanis-mbp.lan>
2022-09-13 18:17:13 -0400

    about me update

--- src/Components/aboutMe/AboutMe.js
+++ src/Components/aboutMe/AboutMe.js
@@ -11,25 +11,20 @@ function AboutMe() {
 			<div className="about__text">
 				<h1>ABOUT ME</h1>
 				<p>
-					I am a mom of two beautiful little girls, who decided after her second
-					child she wanted better options for her career. As a former therapist,
+					I am a mom of two beautiful little girls, I decided after my second
+					child I wanted better options for my career. As a former therapist,
 					spending ten years in the therapy field can really do something to the
-					mind. It can expand the way one solves problems and analyzes
-					obstacles. It can increase your effectiveness in communication and
-					allow you to function in an agile setting. It can also force you to
-					see all the scary that could be, and that is a huge part of why I
-					decided to find my next chapter. I grew up in a time where you chose a
-					career based on wanting a family, it didn't matter that my mind craved
-					math and problem solving. To be an Indian woman meant to be a mother
-					and a wife. Yes, you were encouraged to have an education, but one
-					that allowed for light days and not a busy schedule. In 2021, I took
-					advantage of the bootcamp explosion and decided to try a beginner
-					javascript course. Here I am a year later, full of excitement and fear
-					working towards my new goal: to become a software engineer. This
-					journey has been important for me, not just for me, but for the two
-					girls I am raising. I am showing them that a female is so much more
-					than what the world sees, and I hope that this site has given you a
-					small sense of that. Thanks for visiting.
+					mind. It has expanded the way I solve problems and analyze obstacles.
+					It also taught me the importance of effective two-way communication.
+					It can also force you to see all the scary that could be, and that is
+					a huge part of why I decided to find my next chapter. Here I am a year
+					later, full of excitement and fear working towards my new career:
+					software engineering. This journey has been important for not just me,
+					but for the two girls I am raising. I am showing them that a female is
+					so much more than what roles society expects of you, and that you can
+					make your own path and choices. I hope that this site has given you a
+					small sense of who I am and the value add I would have to your
+					engineering team. Thanks for visiting.
 				</p>
 			</div>
 		</div>
